5601909cd5479816105bbc00269361e1a4a83fbf
Zoltan Varga <Zoltan.Varga@arm.com>
2017-12-27 10:10:37 +0100

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -15,14 +15,6 @@ store.subscribe(() => {
   return console.log(store.getState());
 });
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas bill', amount: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
